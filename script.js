88931a45b8b0c4d13b66e766c22a4f52c5feb4bc
neth1122 <enedehn@gmail.com>
2025-07-07 07:45:48 +0100

    testing

--- script.js
+++ script.js
@@ -17,9 +17,24 @@ author.textContent = "";
 }
 } catch (error) {
 quote.textContent = "Failed to load quote.";
-author.textContent = "";
+author.textContent = "Failed to load author.";
 console.error("Error fetching quote:", error);
 }
 }
 
 getquote(api_url);
+
+const shareBtn = document.getElementById('shareBtn');
+const shareOptions = document.getElementById('share');
+shareBtn.addEventListener('click', () => {
+    shareOptions.classList.toggle('active');
+});
+
+
+const link = 'https://neth1122.github.io/Quote_generator/';
+
+const fb = document.querySelector('.facebook');
+fb.href = `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(link)}&quote=${encodeURIComponent(quote.textContent + " — " + author.textContent)}`;
+
+const twitter = document.querySelector('.twitter');
+twitter.href = `https://twitter.com/intent/tweet?text=${encodeURIComponent(quote.textContent + " — " + author.textContent)}&url=${encodeURIComponent(link)}`;
